1ef5d8c2aff5a52ff6552ace3e5c9970e88dbe4c
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-17 19:26:33 +0000

    running extension

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,21 +3,14 @@ import * as vscode from 'vscode';
 import * as path from "path";
 import { workspace, ExtensionContext } from 'vscode';
 import { LanguageClientOptions, LanguageClient, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import {runPseudo } from './runPseudo';
 
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-	// load the rust tower_lsp server
-	// const serverModule = context.asAbsolutePath(
-	// 	path.join("client", "server")
-	// );
-
 	// if the extension is launched in debug mode then the debug server options are used
 	// otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		// command: path.join(__dirname, "server"),
-		// args: [],
-		// transport: TransportKind.stdio,
 		run: {
 			command: path.join(__dirname, "server"),
 			args: [],
@@ -29,7 +22,6 @@ export function activate(context: vscode.ExtensionContext) {
 			transport: TransportKind.stdio
 		},
 	};
-
 	const clientOptions: LanguageClientOptions = {
 		// register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'pseudo' }],
@@ -49,14 +41,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// start the client. This will also launch the server
 	client.start();
+	vscode.window.showInformationMessage("Pseudo Runner is now active!");
 
-	let disposable = vscode.commands.registerCommand('pseudo-runner.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pseudo runner!');
-	});
+	context.subscriptions.push(vscode.commands.registerCommand('pseudoRunner.runPseudo', runPseudo));
 
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
---
+++ client/src/runPseudo.ts
@@ -0,0 +1,23 @@
+import * as vscode from "vscode";
+import { exec } from 'child_process';
+import * as path from 'path';
+
+// function to run the pseudo code, using the binary
+export const runPseudo = async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const fileName = document.fileName;
+        // check and get a terminal with the name "NASM Terminal"
+        const existingTerminal = vscode.window.terminals.find((terminal) => terminal.name === 'Pseudo Runner');
+
+        if (existingTerminal) {
+            existingTerminal.sendText("clear");
+            existingTerminal.sendText(`${path.join(__dirname, "pseudo_interpreter")} ${fileName}`);
+        } else {
+            // else create a new terminal with the name "NASM Terminal" and send the execCommand
+            const terminal = vscode.window.createTerminal('Pseudo Runner');
+            terminal.show();
+        }
+    }
+};
