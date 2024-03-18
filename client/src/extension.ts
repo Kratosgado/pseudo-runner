373a4833024f6a9e45691ef42799c524d27ce9c5
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-18 16:56:26 +0000

    disable language server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,37 +10,37 @@ let client: LanguageClient;
 export function activate(context: vscode.ExtensionContext) {
 	// if the extension is launched in debug mode then the debug server options are used
 	// otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: {
-			command: path.join(__dirname, "server"),
-			args: [],
-			transport: TransportKind.stdio,
-		},
-		debug: {
-			command: path.join(__dirname, "server"),
-			args: [],
-			transport: TransportKind.stdio
-		},
-	};
-	const clientOptions: LanguageClientOptions = {
-		// register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'pseudo' }],
-		synchronize: {
-			// notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
+	// const serverOptions: ServerOptions = {
+	// 	run: {
+	// 		command: path.join(__dirname, "server"),
+	// 		args: [],
+	// 		transport: TransportKind.stdio,
+	// 	},
+	// 	debug: {
+	// 		command: path.join(__dirname, "server"),
+	// 		args: [],
+	// 		transport: TransportKind.stdio
+	// 	},
+	// };
+	// const clientOptions: LanguageClientOptions = {
+	// 	// register the server for plain text documents
+	// 	documentSelector: [{ scheme: 'file', language: 'pseudo' }],
+	// 	synchronize: {
+	// 		// notify the server about file changes to '.clientrc files contained in the workspace
+	// 		fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+	// 	}
+	// };
 
-	// create the language client and start the client
-	client = new LanguageClient(
-		'pseudoRunner',
-		'Pseudo Runner',
-		serverOptions,
-		clientOptions
-	);
+	// // create the language client and start the client
+	// client = new LanguageClient(
+	// 	'pseudoRunner',
+	// 	'Pseudo Runner',
+	// 	serverOptions,
+	// 	clientOptions
+	// );
 
 	// start the client. This will also launch the server
-	client.start();
+	// client.start();
 	vscode.window.showInformationMessage("Pseudo Runner is now active!");
 
 	context.subscriptions.push(vscode.commands.registerCommand('pseudoRunner.runPseudo', runPseudo));
