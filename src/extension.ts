881804bc6fdd002c892a91b3c5b6f54379465c4b
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-22 20:32:09 +0000

    first debug diagnostic function

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,47 @@
 
-// import * as path from "path";
-// import { workspace, ExtensionContext } from 'vscode';
-// import { LanguageClientOptions, LanguageClient, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import * as path from "path";
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClientOptions, LanguageClient, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
 import * as vscode from 'vscode';
 import { runPseudo } from './runPseudo';
-// let client: LanguageClient;
+
+let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
 	// if the extension is launched in debug mode then the debug server options are used
 	// otherwise the run options are used
-	// const serverOptions: ServerOptions = {
-	// 	run: {
-	// 		command: path.join(__dirname, "server"),
-	// 		args: [],
-	// 		transport: TransportKind.stdio,
-	// 	},
-	// 	debug: {
-	// 		command: path.join(__dirname, "server"),
-	// 		args: [],
-	// 		transport: TransportKind.stdio
-	// 	},
-	// };
-	// const clientOptions: LanguageClientOptions = {
-	// 	// register the server for plain text documents
-	// 	documentSelector: [{ scheme: 'file', language: 'pseudo' }],
-	// 	synchronize: {
-	// 		// notify the server about file changes to '.clientrc files contained in the workspace
-	// 		fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-	// 	}
-	// };
+	const serverOptions: ServerOptions = {
+		run: {
+			command: path.join(__dirname, "pseudo_lsp"),
+			args: [],
+			transport: TransportKind.stdio,
+		},
+		debug: {
+			command: path.join(__dirname, "pseudo_lsp"),
+			args: [],
+			transport: TransportKind.stdio
+		},
+	};
+	const clientOptions: LanguageClientOptions = {
+		// register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'pseudo' }],
+		synchronize: {
+			// notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
 
-	// // create the language client and start the client
-	// client = new LanguageClient(
-	// 	'pseudoRunner',
-	// 	'Pseudo Runner',
-	// 	serverOptions,
-	// 	clientOptions
-	// );
+	// create the language client and start the client
+	client = new LanguageClient(
+		'pseudoRunner',
+		'Pseudo Runner',
+		serverOptions,
+		clientOptions
+	);
 
 	// start the client. This will also launch the server
-	// client.start();
+	client.start();
 	vscode.window.showInformationMessage("Pseudo Runner is now active!");
 
 	context.subscriptions.push(vscode.commands.registerCommand('pseudoRunner.runPseudo', runPseudo));
